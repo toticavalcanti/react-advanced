619ea433106ca0474abe881d02973a8a73ec3b1a
Antonio Cavalcante de Paula Filho (Toti Cavalcanti) <toticavalcanti@hotmail.com>
2021-03-11 00:48:04 -0300

    react life cycle with class

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import Twitter from './Twitter'
 
-class Twitter extends Component {
+class App extends Component {
 
-  render(){
-    return(
-      <div>Teste</div>
+  state = {
+    loading: false,
+    actived: true
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        loading: true
+      })
+    }, 3000)
+  }
+
+  onRemove = () => {
+    this.setState({
+      actived: false
+    })
+  }
+
+  render () {
+    
+    const posts = [{
+      title: 'React',
+      description: 'Tutorial de react'
+    },{
+      title: 'Django',
+      description: 'Tutorial de Django'
+    }]
+
+    return (
+      <div>
+        <button onClick={this.onRemove}>Remover component</button>
+        {this.state.actived && (
+          <Twitter posts={posts} loading={this.state.loading} />
+        )}
+      </div>
     )
   }
 }
 
-export default Twitter;
+export default App
