c0e94226a4dc95423312a102d5c59e31c5cfaaac
Antonio Cavalcante de Paula Filho (Toti Cavalcanti) <toticavalcanti@hotmail.com>
2021-03-15 19:41:08 -0300

    typecheckoing proptypes

--- src/App.js
+++ src/App.js
@@ -1,38 +1,21 @@
-import React, { Component } from 'react'
-import Counter from './Counter'
+import React from 'react'
+import Petshop from './Petshop';
 
-const Buttons = ({ increment, decrement, count }) => (
-  <div>
-      <h1>Valor atual: {count}</h1>
-      <div>
-        <button onClick={increment}>Adicionar 1</button>
-      </div>
-      <div>
-        <button onClick={decrement}>Remover 1</button>
-      </div>
-  </div>
-)
+function App() {
 
-class App extends Component {
-
-  render () {
-    return (
-      <div>
-        <Counter render={
-          ({ increment, decrement, count }) => (
-            <Buttons
-              increment={increment}
-              decrement={decrement}
-              count={count}
-            />
-          )
-        }>
-
-        </Counter>
-      </div>
-    )
+  const handleClick = () => {
+    console.log('Iniciando o banho...')
   }
+
+  return(
+    <div>
+      <Petshop 
+        dogs={2} 
+        customerName = "Toti Cavalcanti"
+        onClick={handleClick}
+        />
+    </div>
+  )
 }
 
-
-export default App
+export default App;
---
+++ src/Petshop.js
@@ -0,0 +1,35 @@
+import React from 'react'; 
+import PropTypes from 'prop-types';
+
+function Petshop(props){
+  const { dogs, cats, customers, customerName, onClick } = props;
+  return(
+    <div>
+      <h1>Dogs: {dogs}</h1>
+      <h1>Cats: {cats}</h1>
+      <div>
+        Quantidade de clientes: {customers}
+      </div>
+      <div>
+        Nome do cliente: {customerName}
+      </div>
+      <button onClick={ onClick }>Iniciar banho</button>
+    </div>
+  )
+}
+
+Petshop.defaultProps = {
+  cats: 0,
+  customers: [],  
+}
+
+Petshop.propTypes = {
+  dogs: PropTypes.number.isRequired,
+  cats: PropTypes.number,
+  customers: PropTypes.array,
+  customerName: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+  status: PropTypes.oneOf(['disabled', 'abled'])
+}
+
+export default Petshop;
