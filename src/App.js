af2d95986f3cf33b6dca2afec7c1f2adcc80c189
Antonio Cavalcante de Paula Filho (Toti Cavalcanti) <toticavalcanti@hotmail.com>
2021-03-15 11:58:03 -0300

    render props

--- src/App.js
+++ src/App.js
@@ -1,34 +1,38 @@
-import React, { Fragment } from 'react'
+import React, { Component } from 'react'
+import Counter from './Counter'
 
-const store = [{
-  type: 'Roupa'
-}, {
-  type: 'Calçado'
-}, {
-  type: 'Camiseta'
-}]
+const Buttons = ({ increment, decrement, count }) => (
+  <div>
+      <h1>Valor atual: {count}</h1>
+      <div>
+        <button onClick={increment}>Adicionar 1</button>
+      </div>
+      <div>
+        <button onClick={decrement}>Remover 1</button>
+      </div>
+  </div>
+)
 
-function Column({ type }){
-  return(
-    <tr>
-      <td>{ type.name.c }</td>
-    </tr>
-  )
+class App extends Component {
+
+  render () {
+    return (
+      <div>
+        <Counter render={
+          ({ increment, decrement, count }) => (
+            <Buttons
+              increment={increment}
+              decrement={decrement}
+              count={count}
+            />
+          )
+        }>
+
+        </Counter>
+      </div>
+    )
+  }
 }
 
-function App(){
 
-  const renderColumns = (element, key) => (
-    <Fragment key={`column-${key}`}>
-      <Column type={element.type} />
-    </Fragment>
-  )
-
-  return(
-    <table>
-        {store.map(renderColumns)}   
-    </table>
-  )
-}
-
-export default App; 
+export default App
--- src/index.js
+++ src/index.js
@@ -3,15 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import ErrorBoundary from './ErrorBoundary';
 
-const main = (
-  <ErrorBoundary>
+ReactDOM.render(
+  <React.StrictMode>
     <App />
-  </ErrorBoundary>
-)
-
-ReactDOM.render(main, document.getElementById('root'));
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
